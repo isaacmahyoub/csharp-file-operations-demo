8ca8bc578ac81ad6ee5bf5b9eaa65da4fedd1f6b
isaacmahyoub <eng.isaacmahyoub@gmail.com>
2025-04-17 18:11:33 +0300

    version first

---
+++ FileManagerApp/FileManagerApp/src/Controllers/FileController.cs
@@ -0,0 +1,51 @@
+﻿using FileManagerApp.src.Models;
+
+namespace FileManagerApp.src.Controllers
+{
+    public class FileController
+    {
+        private readonly FileModel _model;
+        public FileController()
+        {
+            _model = new FileModel();
+        }
+
+
+        public void OpenFile(string filePath)
+        {
+            _model.FilePath = filePath;
+            _model.ReadFile();
+        }
+
+
+        public void SaveFile()
+        {
+            _model.SaveFile();
+        }
+
+        public void CreateFile(string filePath, string content)
+        {
+            _model.FilePath = filePath;
+            _model.Content = content;
+            _model.CreateFile();
+        }
+
+        public void DeleteFile(string filePath)
+        {
+            _model.FilePath = filePath;
+            _model.DeleteFile();
+        }
+
+        public string GetFileContent()
+        {
+            return _model.Content;
+        }
+
+        public void SetFileContent(string content)
+        {
+            _model.Content = content;
+        }
+
+
+    }
+}
